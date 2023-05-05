948ea9f583780d074e5fc6d94769b5651cbdb2ee
CHINEDU EZEOKOYE <c.ezeokoye3.22@abdn.ac.uk>
2023-05-05 21:05:40 +0000

    behave tests

--- features/steps/checkout.py
+++ features/steps/checkout.py
@@ -1,27 +1,49 @@
 import urllib
 from urllib.parse import urljoin
 from behave import given, when, then
+from selenium.webdriver.common.by import By
 
-@given(u'we submit a product')
+@given(u'we search a product')
 def user_on_product_newpage(context):
 	base_url = urllib.request.url2pathname(context.test_case.live_server_url)
 	print(base_url)
-	open_url = urljoin(base_url,'/create/')
+	open_url = urljoin(base_url,'/search/')
 	context.browser.get(open_url)
 
 
-@when(u'we fill in the form')
-def user_fills_in_the_form(context):
-	# use print(context.browser.page_source) to aid debugging
-	# only prints page source if there is an error in the step
+@when(u'we enter the query')
+def user_enters_the_query(context):
 	print(context.browser.page_source)
-	name_textfield = context.browser.find_element('name', 'name')
-	name_textfield.send_keys('thing one')
-	price_textfield = context.browser.find_element('name','price')
-	price_textfield.send_keys(3)
-	context.browser.find_element('name','submit').click()
+	name_textfield = context.browser.find_element('name', 'query')
+	name_textfield.send_keys('K')
+	search_button = context.browser.find_element(By.CSS_SELECTOR,'input[type="submit"]')
+	search_button.click()
 
 
+# @then(u'it succeeds')
+# def step_impl(context):
+# # 	base_url = urllib.request.url2pathname(context.test_case.live_server_url)
+# # 	open_url = urljoin(base_url,"/search/?query=K")
+# # 	context.browser.get(open_url)
+# # 	print(context.browser.page_source)
+# # 	# expected_product = 'Kepler'
+# # 	# id_query = context.browser.find_element(By.NAME,'id_query')
+# # 	# assert expected_product in id_query.text
+# # 	assert 'Kepler' in context.browser.page_source
 @then(u'it succeeds')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then it succeeds')
+    # Search for the product
+    search_box = context.browser.find_element(By.NAME, 'query')
+    search_box.send_keys('Kepler')
+    search_box.submit()
+
+    # Click on the link of the first search result
+    search_results = context.browser.find_elements(By.CSS_SELECTOR, '.product-list-item a')
+    assert len(search_results) > 0, 'No search results found'
+    first_result = search_results[0]
+    detail_url = first_result.get_attribute('href')
+    context.browser.get(detail_url)
+
+    # Check that we are on the product detail page
+    assert 'Kepler' in context.browser.page_source
+
